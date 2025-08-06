ec0f503f2f85b815099f70ab175269bca646f112
casalecchi <felipevilela191@gmail.com>
2025-08-06 13:26:03 -0300

    create first component: use Image component and style Text and View

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,37 @@
-import { StyleSheet } from "react-native";
-import { Text, View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
+import { StyleSheet, Text, Image, View } from "react-native";
+import products from "@/assets/data/products";
+
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text>Teste</Text>
+      <Image style={styles.image} source={{ uri: product.image }} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 600,
+    marginVertical: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  price: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: Colors.light.tint,
   },
 });
